500d01aca10e0af31d4ce07c5b395ad3a4f92180
fdev31 <fdev31@gmail.com>
2019-08-19 20:47:12 +0200

    code cleanup

--- examples/test.py
+++ examples/test.py
@@ -1,25 +1,32 @@
+HOST="crava.ch"
+USER='pifou'
+PASS='plop'
+TOPIC="rooms/main/+"
+
 import bottle
 from bottle import route, run, template
 
 import paho.mqtt.client as mqtt
 import sys
 
-def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+def init_mqtt():
 
-broker_address="crava.ch"
-#broker_address="iot.eclipse.org" #use external broker
-client = mqtt.Client("P1") #create new instance
-client.on_message=on_message
-client.username_pw_set("pifou", "plop")
-client.connect(broker_address) #connect to broker
-client.loop_start()    #start the loop
+    def on_message(client, userdata, message):
+        print("message received " ,str(message.payload.decode("utf-8")))
+        print("message topic=",message.topic)
+        print("message qos=",message.qos)
+        print("message retain flag=",message.retain)
 
-client.subscribe("trucdetest/jo/test")
-client.loop_stop() #stop the loop
+    client = mqtt.Client("base") #create new instance
+
+    client.on_message = on_message
+    client.username_pw_set(USER, PASS)
+
+    client.connect(HOST)
+
+    client.loop_start()    #start the loop
+    client.subscribe(TOPIC)
+    return client
 
 # http routing
 
@@ -27,11 +34,9 @@ client.loop_stop() #stop the loop
 def index():
     return template('./templates/index.html')
 
-
-@bottle.post('/say')
-def say():
-    text = bottle.request.POST['text']
-    client.publish("trucdetest/jo/test",text) #publish
-
-
-run(host='localhost', port=8080)
+mqtt_runner = init_mqtt()
+try:
+    run(host='localhost', port=8080)
+except Exception as e:
+    print("byebye")
+    mqtt_runner.loop_stop()
